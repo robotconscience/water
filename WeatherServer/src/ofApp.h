db9b010bb5c24a54a181e0585588d0da9b1734f6
Brett Renfer <brett@robotconscience.com>
2014-06-12 10:25:55 -0400

    Fixed updated addons

--- WeatherServer/src/ofApp.h
+++ WeatherServer/src/ofApp.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxFluid.h"
+#include "ofxSpacebrew.h"
+#include "ofxRCUtils.h"
+#include "ofxLabFlexParticleSystem.h"
 
 class ofApp : public ofBaseApp{
 
@@ -18,5 +22,24 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+
+        // server
+    
+        // wind simulation
+        ofLight light;
+        ofMesh sphere;
+        ofxFluid fluid;
+        
+        ofVec2f oldM;
+        int     width,height;
+        bool    bPaint, bObstacle, bBounding, bClear;
+    
+        ofPixels velPix;
+        bool    bNeedToRead;
+        ofVec2f getVelocityLatLong( float lat, float lon );
+        ofVec2f getVelocity( float x, float y );
+    
+        // files
+        ofFbo fileFBO, screenFBO;
+        lab::ParticleSystem files;
 };
