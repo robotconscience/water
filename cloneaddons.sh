f0a16fb72aab38004e41f9a1b9952741f6f39344
Brett Renfer <brett@robotconscience.com>
2014-06-12 10:30:45 -0400

    Quick clone addons shell

---
+++ cloneaddons.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ../../addons
+PREFIX="git clone https://github.com/"
+
+${PREFIX}robotconscience/ofxLabFlexParticleSystem
+${PREFIX}patriciogonzalezvivo/ofxFluid
+${PREFIX}patriciogonzalezvivo/ofxFx
+${PREFIX}labatrockwell/ofxLibwebsockets
+${PREFIX}spacebrew/ofxSpacebrew
+${PREFIX}rezaali/ofxUI
